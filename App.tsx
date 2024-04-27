12483f35af802bac7c227b401b97b8834f7b3771
bezbrain <belloquadriolawale@gmail.com>
2024-04-27 13:14:03 +0100

    Fix app loading bug

--- App.tsx
+++ App.tsx
@@ -3,17 +3,32 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
 import { AuthProvider, useAuthContext } from "./context/auth.context";
-import { useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import AppLoading from "expo-app-loading";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 const Stack = createNativeStackNavigator();
+
+interface NavigationProps {
+  isTryingLogin: boolean;
+  setIsTryingLogin: Dispatch<SetStateAction<boolean>>;
+}
 
 // AUTHENTICATION SCREENS
 function AuthStack() {
@@ -47,11 +62,38 @@ function AuthenticatedStack() {
 }
 
 // CONDITIONAL RENDERING OF SCREENS
-function Navigation() {
+function Navigation({ isTryingLogin, setIsTryingLogin }: NavigationProps) {
   const { authToken } = useAuthContext();
 
+  // HANDLE INTIAL SCREEN DISPLAY WHEN ALL FONTS HAVE NOT BEEN LOADED
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsTryingLogin(false);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (!isTryingLogin) {
+      // This tells the splash screen to hide immediately!
+      await SplashScreen.hideAsync();
+    }
+  }, [isTryingLogin]);
+
+  // This is to make sure that if a user was initially logged in and the app is reaccessed, the delay caused by useEffect does not make the login in page show before the expected page if a user is logged in
+  if (isTryingLogin) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onLayoutRootView}>
       {!authToken ? <AuthStack /> : <AuthenticatedStack />}
     </NavigationContainer>
   );
@@ -77,12 +119,12 @@ function Root() {
     fetchToken();
   }, []);
 
-  // This is to make sure that if a user was initially logged in and the app is reaccessed, the delay caused by useEffect does not make the login in page show before the expected page if a user is logged in
-  if (isTryingLogin) {
-    return <AppLoading />;
-  }
-
-  return <Navigation />;
+  return (
+    <Navigation
+      isTryingLogin={isTryingLogin}
+      setIsTryingLogin={setIsTryingLogin}
+    />
+  );
 }
 
 export default function App() {
