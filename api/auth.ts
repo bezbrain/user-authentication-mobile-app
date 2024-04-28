e6421c2a055952b19f24f59beb0745b20e733478
bezbrain <belloquadriolawale@gmail.com>
2024-04-28 19:50:01 +0100

    Adjust the app.json file

--- api/auth.ts
+++ api/auth.ts
@@ -3,12 +3,18 @@ import baseUrl from "../utils/config";
 
 // REGISTER A USER
 export const registerUser = async (user: Object) => {
-  const { data } = await axios.post(`${baseUrl.baseUrl}/register`, user);
+  const { data } = await axios.post(
+    `https://minimag.onrender.com/api/v1/register`,
+    user
+  );
   return data;
 };
 
 // LOGIN A USER
 export const loginUser = async (user: Object) => {
-  const { data } = await axios.post(`${baseUrl.baseUrl}/login`, user);
+  const { data } = await axios.post(
+    `https://minimag.onrender.com/api/v1/login`,
+    user
+  );
   return data;
 };
